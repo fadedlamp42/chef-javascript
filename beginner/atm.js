bc369bf5672f838f84017a372495b8776c7b5971
Peter Gomez <fadedlamp42@gmail.com>
2021-05-31 19:50:12 -0400

    solved ATM

---
+++ beginner/atm.js
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/HS08TEST
+
+input = ""
+process.stdin.on('data', (d) => input += d)
+process.stdin.on('end', () => {
+    input = input.split('\n')
+    main()
+})
+
+function main() {
+    let [withdraw, balance] = input[0].split(' ').map(Number)
+    withdraw += 0.5
+
+    if(withdraw <= balance && withdraw%5 === 0.5)
+        process.stdout.write((balance - withdraw).toFixed(2))
+    else
+        process.stdout.write(balance.toFixed(2))
+
+    process.stdout.write('\n')
+}
